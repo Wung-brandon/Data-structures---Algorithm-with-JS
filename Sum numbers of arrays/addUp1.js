57076c49ae3519966bf9b2205e33aa049a6c4043
Wung Brandon <wungbrandon27@gmail.com>
2024-10-26 22:50:21 +0100

    added some algorithm code

--- Sum numbers of arrays/addUp1.js
+++ Sum numbers of arrays/addUp1.js
@@ -10,3 +10,43 @@ console.log(addUpTo(5)); // Output: 15
 
 // The big O notation of this function is O(n) where n is the number
 // The number of operations is eventually bounded by a multiple of n (say, 10n)
+
+function printAllPairs(n){
+    for (var i=0; i < n; i++) {
+        for (var j=0; j < n; j++) {
+            // console.log(i, j);
+        }
+    }
+}
+
+console.log(printAllPairs(5)); // Output: 25 pairs (5*5)
+
+function logAtLeast5(n){
+    for (var i = 1; i <= Math.max(5, n); i++) {
+        console.log(i);
+    }
+}
+
+console.log(logAtLeast5(3)); // Output: 1, 2, 3, 4, 5
+
+function logAtMost5(n){
+    for (var i = 1; i <= Math.min(5, n); i++) {
+        console.log(i);
+    }
+}
+
+console.log(logAtMost5(7)); // Output: 1, 2, 3, 4, 5
+
+function onlyElementsAtEvenIndex(array){
+    var newArray = Array(Math.ceil(array.length / 2))
+    // let evenArray = []
+    for (var i = 0; i < array.length; i++){
+        if (i % 2 === 0) {
+            // evenArray.push(array[i])
+            newArray[i / 2] = array[i]
+        }
+    }
+    // return evenArray
+    return newArray
+}
+console.log(onlyElementsAtEvenIndex([1, 2, 5, 7, 6, 8, 10, 14, 7]))
--- Sum numbers of arrays/sum_numbers.js
+++ Sum numbers of arrays/sum_numbers.js
@@ -6,3 +6,26 @@ function addNumbers(n){
     return total;
 }
 console.log(addNumbers[1,2,3])
+
+function double(arr){
+    let newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2);
+    }
+    return newArr;
+}
+
+console.log(double([1, 2, 3, 4, 5]));
+
+let instructor = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    courses: ['JavaScript', 'React', 'Node.js'],
+    isStudent: true,
+    grades: [85, 92, 78],
+}
+console.log(Object.keys(instructor))
+console.log(Object.values(instructor))
+console.log(Object.entries(instructor))
+console.log(Object.hasOwnProperty('lastName'))
